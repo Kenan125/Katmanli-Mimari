82f8a79d081451a619dfc88009cdd724f5f0c8e6
Kanan Ismayilzada <kenanismayilzade12@gmail.com>
2024-07-27 22:21:26 +0300

    added core project
    some changes

--- Entities/Concrete/Order.cs
+++ Entities/Concrete/Order.cs
@@ -1,17 +1,19 @@
 ﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entities.Concrete
 {
-    public class Customer:IEntity
+    public class Order : IEntity
     {
+        public int OrderId { get; set; }
         public string CustomerId { get; set; }
-        public string ContactName { get; set; }
-        public string CompanyName { get; set; }
-        public string City { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string ShipCity { get; set; }
     }
 }
